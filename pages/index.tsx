6974e312c7d1964ade3b82573b0d3c3a8bcb14b8
Thanaen <Thanaen@users.noreply.github.com>
2021-01-12 22:36:53 +0100

    Use react-typist to display text

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Head from 'next/head';
+import Typist from 'react-typist';
+import 'react-typist/dist/Typist.css';
 import dayjs from 'dayjs';
 import isBetween from 'dayjs/plugin/isBetween';
 
 dayjs.extend(isBetween);
 
-const Home: React.FC = () => {
+const getMessage = () => {
   const hour = dayjs().hour();
   let message: string | undefined;
   if (hour >= 20 && hour < 22) {
@@ -20,12 +22,21 @@ const Home: React.FC = () => {
     message = 'Nope.';
   }
 
+  return message;
+};
+
+const Home: React.FC = () => {
   return (
     <>
       <Head>
         <title>Should I go to sleep ?</title>
       </Head>
-      <span>{message}</span>
+      <span>
+        <Typist>
+          Mmmmh...
+          <Typist.Delay ms={500} /> {getMessage()}
+        </Typist>
+      </span>
     </>
   );
 };
